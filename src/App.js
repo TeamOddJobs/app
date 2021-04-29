897fde71141f2d88fde0d653f4202e363b48d451
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-28 18:45:13 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import MovieHeader from './components/itemheader';
-import MovieList from './components/itemlist';
-import Movie from './components/item';
+import ItemHeader from './components/itemheader';
+import ItemList from './components/itemlist';
+import Item from './components/item';
 import Authentication from './components/authentication';
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';

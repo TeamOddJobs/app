c754f4519f7e6160104adee61d5ebaf04b9308d1
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-30 03:55:34 -0600

    Update Buy.js

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import {Card, ListGroup, ListGroupItem, Button, Form } from 'react-bootstrap';
 import Receipt from "./Reciept";
 

fa78348c73e98803c3d6f3a069e57d584ed33618
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-30 02:42:20 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import ItemList from './components/itemlist';
 import Item from './components/item';
 import Authentication from './components/authentication';
 import buy from './components/buy';
-import Reciept from './components/Reciept';
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/store';
 
 function App() {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <HashRouter>
-          <div>
-            <ItemHeader />
-            <Route exact path="/" render={()=><ItemList />}/>
-            <Route exact path="/itemlist" render={()=><ItemList />}/>
-            <Route exact path="/item/:itemId" render={()=><Item />}/>
-            <Route path="/signin" render={()=><Authentication />}/>
-            <Route eaxct path="/buy" render={()=><buy />}/>
-            <Route exact path="/Reciept" />
-          </div>
-        </HashRouter>
-      </Provider>
-    </div>
+      <div className="App">
+        <Provider store={store}>
+          <HashRouter>
+            <div>
+              <ItemHeader />
+              <Route exact path="/" render={()=><ItemList />}/>
+              <Route exact path="/itemlist" render={()=><ItemList />}/>
+              <Route exact path="/item/:itemId" render={()=><Item />}/>
+              <Route path="/signin" render={()=><Authentication />}/>
+              <Route eaxct path="/buy" render={()=><buy />}/>
+              <Route exact path="/Reciept" />
+            </div>
+          </HashRouter>
+        </Provider>
+      </div>
   );
 }
 

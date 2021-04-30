af44e791ac8f2c7143ad0aa34998dbd6a30dd503
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-30 08:09:46 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ItemList from './components/itemlist';
 import Item from './components/item';
 import Authentication from './components/authentication';
 import Buy from './components/Buy';
+import Reciept from "./components/Reciept";
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/store';

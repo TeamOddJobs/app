5df5cc2f0374a9d3e5ec6eccf753fda933e656db
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-30 08:20:13 -0600

    Update Reciept.js

--- src/components/Reciept.js
+++ src/components/Reciept.js
@@ -1,33 +1,50 @@
+import React, { Component } from 'react';
 import { Image } from 'react-bootstrap';
+import {connect} from 'react-redux';
 import {Card, ListGroup, ListGroupItem, Button} from 'react-bootstrap';
 
 
 
 
-const Receipt =(props) => {
-        return(
+class Reciept extends Component {
+    render() {
+        return (
             <Card>
                 <Card.Header> Receipt</Card.Header>
                 <Card.Body>
-                    <Image className="image" src={props.image} thumbnail />
+                    <Image className="image" src={this.props.image} thumbnail/>
                 </Card.Body>
                 <ListGroup>
-                    <ListGroupItem>{"Item Name: " + props.itemName}</ListGroupItem>
-                    <ListGroupItem>{"Item cost: $" + props.price}</ListGroupItem>
-                    <ListGroupItem>{"Item Desc: " + props.desc}</ListGroupItem>
-                    <ListGroupItem>{"Name of buyer: " + props.name}</ListGroupItem>
-                    <ListGroupItem>{"Shipping Address: " + props.shipAdr}</ListGroupItem>
-                    <ListGroupItem>{"Total Payed: " + props.payedAmt}</ListGroupItem>
-                    <ListGroupItem>{"Making Donation?:" + props.donateStatus}</ListGroupItem>
-                    <ListGroupItem>{"Total amount Donated: " + props.donateAmt}</ListGroupItem>
-                    <ListGroupItem>{"Charity donated to: " + props.charity}</ListGroupItem>
+                    <ListGroupItem>{"Item Name: " + this.props.selectedItem.itemName}</ListGroupItem>
+                    <ListGroupItem>{"Item cost: $" + this.props.selectedItem.itemPrice}</ListGroupItem>
+                    <ListGroupItem>{"Item Desc: " + this.props.selectedItem.itemDesc}</ListGroupItem>
+                    <ListGroupItem>{"Name of buyer: " + this.props.name}</ListGroupItem>
+                    <ListGroupItem>{"Shipping Address: " + this.props.shipAdr}</ListGroupItem>
+                    <ListGroupItem>{"Total Payed: " + this.props.payedAmt}</ListGroupItem>
+                    <ListGroupItem>{"Making Donation?:" + this.props.donatedStatus}</ListGroupItem>
+                    <ListGroupItem>{"Total amount Donated: " + this.props.donatedAmt}</ListGroupItem>
+                    <ListGroupItem>{"Charity donated to: " + this.props.charity}</ListGroupItem>
                 </ListGroup>
 
 
-                <Button onClick={() => {window.location.href='https://webapi-oddjobs.herokuapp.com'}}>Return to home</Button>
+                <Button onClick={() => {
+                    ;window.location.href = 'https://webapi-oddjobs.herokuapp.com'
+                }}>Return to home</Button>
             </Card>
         )
     }
+}
+const mapStateToProps = state => {
+    return {
+        selectedItem: state.item.selectedItem,
+        name: state.item.name,
+        cardNum: state.item.cardNum,
+        charity: state.item.charity,
+        payedAmt: state.item.payedAmt,
+        donatedAmt: state.item.donatedAmt,
+        donatedStatus: state.item.donatedStatus,
+        shipAdr: state.item.shipAdr
+    }
+}
 
-
-export default Receipt;
+export default connect(mapStateToProps)(Buy);

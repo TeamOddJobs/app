f14340072ddd5eff1447ef5f4d3e1886f25ceaf4
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-30 02:37:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import ItemHeader from './components/itemheader';
 import ItemList from './components/itemlist';
 import Item from './components/item';
-import Authentication from './components/authentication'
+import Authentication from './components/authentication';
+import buy from './components/buy';
+import Reciept from './components/Reciept';
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/store';
@@ -19,6 +21,8 @@ function App() {
             <Route exact path="/itemlist" render={()=><ItemList />}/>
             <Route exact path="/item/:itemId" render={()=><Item />}/>
             <Route path="/signin" render={()=><Authentication />}/>
+            <Route eaxct path="/buy" render={()=><buy />}/>
+            <Route exact path="/Reciept" />
           </div>
         </HashRouter>
       </Provider>

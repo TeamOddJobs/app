d91d6e70b7a184762b9ea6be88e3eada7bcd86cb
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-05-03 17:01:23 -0600

    Update Buy.js

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -64,7 +64,7 @@ class Buy extends Component {
 
         if (this.state.charity !== 'N/A') {
             //let roundup = Math.ceil(parseInt(this.props.selectedItem.itemPrice)/3)*3;
-            let donation = (Math.ceil(parseFloat(this.props.selectedItem.itemPrice)) - parseFloat(this.props.selectedItem.itemPrice));
+            let donation = (Math.ceil(parseFloat(this.props.selectedItem.itemPrice)) - parseFloat(this.props.selectedItem.itemPrice)).toFixed(2);
             let newPrice = donation + parseFloat(this.props.selectedItem.itemPrice);
             this.state.payedAmt = newPrice.toString();
             this.state.donationStatus = 'Yes';
